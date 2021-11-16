858b1999dde19676c4b82cca95e1e1497c793d6e
yijun wang <CWang@machealth.com.au>
2021-11-16 23:24:47 +1100

    V1.1

--- Inventory.WCF.Service/InventoryAndOrderService.cs
+++ Inventory.WCF.Service/InventoryAndOrderService.cs
@@ -10,11 +10,13 @@ using System.Threading.Tasks;
 
 namespace Inventory.WCF.Service
 {
+    //[ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Multiple)]
     public class InventoryAndOrderService : IInventoryAndOrderService
     {
-        int sleep = 5000;
         OrderDataService _orderDataService;
         StockDataService _stockDataService;
+        private static Dictionary<Guid, IInventoryAndOrderServiceCallBack> clients = new Dictionary<Guid, IInventoryAndOrderServiceCallBack>();
         public InventoryAndOrderService()
         {
             _orderDataService = new OrderDataService();
@@ -24,7 +26,7 @@ namespace Inventory.WCF.Service
 
         public bool CreateOrder(Guid stockid, string productname, int quantity)
         {
-            Thread.Sleep(5000);
+            Thread.Sleep(1500);
             //add new order to order list
             var orderUpdateResult = _orderDataService.Add(new Order() { Id = Guid.NewGuid(), ProductName = productname, Quantity = quantity });
             var stock = GetStockById(stockid);
@@ -32,28 +34,78 @@ namespace Inventory.WCF.Service
             stock.Quantity = newQuantity;
             //update stock list
             var stockUpdateResult = _stockDataService.Update(stock).Where(i => i.Id == stock.Id).FirstOrDefault().Quantity == newQuantity ? true : false;
-
-            //notice client to update stock and order list
-            IInventoryAndOrderServiceCallBack callBack = OperationContext.Current.GetCallbackChannel<IInventoryAndOrderServiceCallBack>();
-            callBack.StockQuantityChanged();
+            //send notic to all clients
+            BroadcastMessage();
+            //callBack.StockQuantityChanged();
             return orderUpdateResult && stockUpdateResult;
         }
 
+        private void BroadcastMessage()
+        {
+            // Call each client's callback method
+            ThreadPool.QueueUserWorkItem
+            (
+                delegate
+                {
+                    lock (clients)
+                    {
+                        List<Guid> disconnectedClientGuids = new List<Guid>();
+                        foreach (KeyValuePair<Guid, IInventoryAndOrderServiceCallBack> client in clients)
+                        {
+                            try
+                            {
+                                client.Value.StockQuantityChanged();
+                            }
+                            catch (Exception)
+                            {
+                                // TODO: Better to catch specific exception types.                     
+                                // If a timeout exception occurred, it means that the server
+                                // can't connect to the client. It might be because of a network
+                                // error, or the client was closed  prematurely due to an exception or
+                                // and was unable to unregister from the server. In any case, we 
+                                // must remove the client from the list of clients.
+                                // Another type of exception that might occur is that the communication
+                                // object is aborted, or is closed.
+                                // Mark the key for deletion. We will delete the client after the 
+                                // for-loop because using foreach construct makes the clients collection
+                                // non-modifiable while in the loop.
+                                disconnectedClientGuids.Add(client.Key);
+                            }
+                        }
+                        foreach (Guid clientGuid in disconnectedClientGuids)
+                        {
+                            clients.Remove(clientGuid);
+                        }
+                    }
+                }
+            );
+        }
+
+
         public IEnumerable<Order> GetOrders()
         {
-            Thread.Sleep(5000);
+            Thread.Sleep(1500);
             return _orderDataService.Get();
         }
 
         public Stock GetStockById(Guid id)
         {
-            Thread.Sleep(5000);
+            Thread.Sleep(1500);
             return _stockDataService.Get(id);
         }
 
         public IEnumerable<Stock> GetStocks()
         {
-            Thread.Sleep(5000);
+            Thread.Sleep(1500);
+            //notice client to update stock and order list
+            IInventoryAndOrderServiceCallBack callBack = OperationContext.Current.GetCallbackChannel<IInventoryAndOrderServiceCallBack>();
+            if (callBack != null)
+            {
+                lock (clients)
+                {
+                    clients.Add(Guid.NewGuid(), callBack);
+                }
+            }
             return _stockDataService.Get();
         }
     }
